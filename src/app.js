1c5c08cb4615681e5a377d3e64326d3c4219813f
Luis Gabriel Janco <janco7249@gmail.com>
2023-11-22 12:05:59 -0400

    feat: config routes with comment

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-import authRoutes from './routes/auth.routes'
-import userRoutes from './routes/user.routes'
-import districtRoutes from './routes/district.routes'
-import institutionRoutes from './routes/institution.routes'
-import procedureRoutes from './routes/procedure.routes'
+// import authRoutes from './routes/auth.routes'
+// import userRoutes from './routes/user.routes'
+// import districtRoutes from './routes/district.routes'
+// import institutionRoutes from './routes/institution.routes'
+// import procedureRoutes from './routes/procedure.routes'
 
-import { FRONTEND_URL } from './config'
+import { FRONTEND_URL } from './config.js'
 
 const app = express()
 
@@ -18,8 +18,10 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('/api/auth', authRoutes)
-app.use('/api/user', userRoutes)
-app.use('/api/district', districtRoutes)
-app.use('/api/institution', institutionRoutes)
-app.use('/api/procedure', procedureRoutes)
+// app.use('/api/auth', authRoutes)
+// app.use('/api/user', userRoutes)
+// app.use('/api/district', districtRoutes)
+// app.use('/api/institution', institutionRoutes)
+// app.use('/api/procedure', procedureRoutes)
+
+export default app
