5675a77d0343dfcb00c79173ff99c45b95144fcc
Luis Gabriel Janco <janco7249@gmail.com>
2023-11-22 12:06:39 -0400

    feat: set dotenv in config constants

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 export const PORT = process.env.PROT || 4000
 export const MONGODB_URI = process.env.MONGODB_URI
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'secret'
