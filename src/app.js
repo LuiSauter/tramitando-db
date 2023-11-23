87d79a9f38f12aa9e8cdfc3fb99f91f58e513006
Luis Gabriel Janco <janco7249@gmail.com>
2023-11-22 21:28:39 -0400

    refactor: use api auth

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
-// import authRoutes from './routes/auth.routes'
-// import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes.js'
+// import userRoutes from './routes/user.routes.js'
+// import neighborhoodRoutes from './routes/neighborhood.routes.js'
 // import districtRoutes from './routes/district.routes'
 // import institutionRoutes from './routes/institution.routes'
 // import procedureRoutes from './routes/procedure.routes'
@@ -18,8 +19,9 @@ app.use(cors({
 app.use(express.json())
 app.use(cookieParser())
 
-// app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes)
 // app.use('/api/user', userRoutes)
+// app.use('/api/district', neighborhosodRoutes)
 // app.use('/api/district', districtRoutes)
 // app.use('/api/institution', institutionRoutes)
 // app.use('/api/procedure', procedureRoutes)
